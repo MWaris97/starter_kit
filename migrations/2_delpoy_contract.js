f39a919b8ea820c933e1bb6a988d9380e35061ed
Muhammad Waris <16cs44@students.muet.edu.pk>
2022-01-09 19:22:32 +0500

    Transfer tokens to the exchange contract

--- migrations/2_delpoy_contract.js
+++ migrations/2_delpoy_contract.js
@@ -1,7 +1,17 @@
 const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(Token);
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await   deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  // get total supply of Token
+  await token.totalSupply().then(async (supply) => {
+    // Transfer all the tokens
+    await token.transfer(ethSwap.address, supply.toString());
+  });
+
 };
